4093fc44568f6369c2501af70a42df4f2ad465a0
Lim <wi258@naver.com>
2020-09-13 22:30:46 +0900

    Finished Author Routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
+
 router.get('/', async (req, res) => {
     let books
     try {
-        books = await Books.find().sort({ createAt: 'desc' }).limit(10).exec()
-
+        books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
     } catch {
         books = []
-
     }
     res.render('index', { books: books })
 })
